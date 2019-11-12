060c43ead75967c1e632c5dc7ff90b3a692a6512
Nadeem Anvaritafti <nanvari@live.com>
2019-11-12 13:53:21 -0600

    added login component and setup simple routing

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Route exact path='/' component={LoginForm} />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+
+const LoginForm = props => {
+  
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = e => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+
+            <label>Username:
+                <input
+                    type="text" 
+                    name="username"
+                    placeholder="Username" 
+                    value={credentials.username} 
+                    onChange={handleChange} 
+                    required 
+                />
+            </label>
+
+            <label>Password:
+                <input
+                    type="password" 
+                    name="password"
+                    placeholder="Password" 
+                    value={credentials.password} 
+                    onChange={handleChange} 
+                    required 
+                />
+            </label>
+            <div>
+                <button type='submit'>Submit</button>
+            </div>
+
+        </form>
+        </div>
+    );
+}
+
+
+export default LoginForm;
