496f7e949bd7168c1ddafc8271665f3d1262f6d0
Nadeem Anvaritafti <nanvari@live.com>
2019-11-12 16:14:44 -0600

    added Nav component

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,11 +3,13 @@ import {Route} from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import PrivateRoute from './components/PrivateRoute';
 import Friends from './components/Friends';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Route exact path='/' component={LoginForm} />
       <PrivateRoute exact path='/friends' component={Friends} />
     </div>
