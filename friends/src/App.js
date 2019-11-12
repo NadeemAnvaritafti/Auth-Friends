6805dcc31683808a4875973de0bf4b1dd26b52ff
Nadeem Anvaritafti <nanvari@live.com>
2019-11-12 14:05:10 -0600

    added axiosWithAuth

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import LoginForm from './components/LoginForm';
+import PrivateRoute from './components/PrivateRoute';
+import Friends from './components/Friends';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={LoginForm} />
-
+      <PrivateRoute exact path='/friends' component={Friends} />
     </div>
   );
 }
